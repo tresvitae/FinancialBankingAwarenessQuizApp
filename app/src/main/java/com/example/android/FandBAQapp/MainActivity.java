e4b820eb7e0384ea98de0d1fa70d9951fb2b21b2
tresvitae <tresvitae@hotmail.com>
2018-03-07 20:06:12 +0100

    add private int totalPoints; rename package financialbankingawarenessquizapp to FandBAQapp; edd editText for enter a name; change question 6 as open respond (edittext);

--- app/src/main/java/com/example/android/FandBAQapp/MainActivity.java
+++ app/src/main/java/com/example/android/FandBAQapp/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     RadioButton question3Answer3;
     RadioButton question4Answer1;
     RadioButton question5Answer3;
-    RadioButton question6Answer1;
     RadioButton question7Answer3;
     RadioButton question8Answer3;
     RadioButton question10Answer2;
@@ -36,19 +35,20 @@ public class MainActivity extends AppCompatActivity {
         question3Answer3 = findViewById(R.id.q3_answer3);
         question4Answer1 = findViewById(R.id.q4_answer1);
         question5Answer3 = findViewById(R.id.q5_answer3);
-        question6Answer1 = findViewById(R.id.q6_ans_ashley);
         question7Answer3 = findViewById(R.id.q7_answer3);
         question8Answer3 = findViewById(R.id.q8_answer3);
         question10Answer2 = findViewById(R.id.q10_answer2);
 
         /**
-         * Implemented for readability.
+         * This code count question of number 6 if answer is correct.
          */
-        CheckBox question9Answer2 = findViewById(R.id.q9_answer2);
-        boolean hasQuestion9Answer2 = question9Answer2.isChecked();
+        EditText answer = findViewById(R.id.q6_answer);
+        String textAnswer = answer.getText().toString().trim().toLowerCase();
+        if (textAnswer.equalsIgnoreCase("Ashley"))
+            totalPoints += 1;
 
         /**
-         * This code count questions if are true.
+         * This code count question of number 9 if are true.
          */
         CheckBox question9Answer1 = findViewById(R.id.q9_answer1);
         boolean hasQuestion9Answer1 = question9Answer1.isChecked();
@@ -58,9 +58,15 @@ public class MainActivity extends AppCompatActivity {
         boolean hasQuestion9Answer3 = question9Answer3.isChecked();
         Log.v("MainActivity", "Correct q9Option1: " + hasQuestion9Answer3);
 
+        CheckBox question9Answer2 = findViewById(R.id.q9_answer2);
+        boolean hasQuestion9Answer2 = question9Answer2.isChecked();
+
         if (hasQuestion9Answer1 && hasQuestion9Answer3 && !hasQuestion9Answer2)
             totalPoints += 1;
 
+        /**
+         * This code count rest questions if are true.
+         */
         if (question1Answer2.isChecked())
             totalPoints += 1;
         Log.v("MainActivity", "Correct question1Answer2: " + question1Answer2.isChecked());
@@ -80,10 +86,6 @@ public class MainActivity extends AppCompatActivity {
         if (question5Answer3.isChecked())
             totalPoints += 1;
         Log.v("MainActivity", "Correct question5Answer3: " + question5Answer3.isChecked());
-
-        if (question6Answer1.isChecked())
-            totalPoints += 1;
-        Log.v("MainActivity", "Correct question6Answer1: " + question6Answer1.isChecked());
 
         if (question7Answer3.isChecked())
             totalPoints += 1;
